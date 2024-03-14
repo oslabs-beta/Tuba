358b7f4e6e65b2a5d317b5dbdb4bb68f48ddaa2b
Myles Austin <mtv1243@gmail.com>
2024-03-13 22:21:41 -0400

    create routes and controllers for getting error data from the db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const errorDataRouter = require('./routes/errorDataRoute')
 
+app.use(express.json());
 
 
 app.get('/server', (req, res) => {
@@ -9,6 +11,21 @@ app.get('/server', (req, res) => {
     res.json('Hello World!')
 })
 
+app.use('/errorData', errorDataRouter)
+
+
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'An error occurred' },
+    };
+  
+    
+    const errorObj = Object.assign(defaultErr, err);
+    console.log(errorObj.log);
+    res.status(errorObj.status).json(errorObj.message);
+  });
 
 
 app.listen(port, () => {
