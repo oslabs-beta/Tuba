69d67f58a64fd5ffa00da4a8ff7d50fa50dd1bfe
rmarkowi1990 <rick@fastcasual.co>
2024-04-02 19:23:15 -0700

    now with white

--- src/Components/Timeline/TimelineNode.jsx
+++ src/Components/Timeline/TimelineNode.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
-export default function TimelineNode({ handler, id, percentage, letter, color, cascade }) {
+export default function TimelineNode({ data, handler, id, percentage, letter, color, cascade }) {
+
+
+
+
+    const modId = (Number(data.err_srv_id) * 10) % 10;
+
+    const colors = [
+        "#FF6347", // Tomato
+        "#FF4500", // Orange Red
+        "#FF8C00", // Dark Orange
+        "#FFD700", // Gold
+        "#ADFF2F", // Green Yellow
+        "#32CD32", // Lime Green
+        "#4169E1", // Royal Blue
+        "#8A2BE2", // Blue Violet
+        "#FF1493", // Deep Pink
+        "#A0522D"  // Sienna
+    ]
 
 
     const style = {
         marginLeft: percentage,
-        backgroundColor: color,
+        backgroundColor: colors[modId],
 
     }
 
