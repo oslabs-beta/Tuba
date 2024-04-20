6e94443bb77465045de745fb720496f94d535336
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2024-04-20 14:15:15 -0400

    configured jest and supertest for server routes, created 9 passing tests for all routes

---
+++ tests/errorDataTests.test.js
@@ -0,0 +1,114 @@
+const request = require('supertest');
+const server = require('../server/server');
+const db = require('../server/models/tubaDB');
+
+
+afterAll(async () => {
+  await db.end();
+  await server.close();
+})
+
+describe('Server Route Tests', () => {
+  
+  describe('Root path fetch', () => {
+
+    describe("test: /", () => {
+      it("request successful to fetch index.html from 'dist' build folder", async () => {
+        const response  = await request(server)
+          .get('/')
+          .expect(200);
+      });
+    });
+  });
+
+  describe('Integration testing errorData routes', () => {
+    
+    describe('test: /errorData/allData', () => {
+      it('responds with 200 status and application/json content type', async () => {
+        await request(server)
+          .get('/errorData/allData')
+          .expect('Content-Type', /application\/json/)
+          .expect(200);
+      });
+
+      it('errors, services and connections are in body of response', async () => {
+        const response  = await request(server)
+          .get('/errorData/allData')
+        expect(Array.isArray(response.body.services)).toBe(true)
+        expect(Array.isArray(response.body.errors)).toBe(true)
+        expect(Array.isArray(response.body.connections)).toBe(true)
+        expect(response.body.services.every(item => typeof item === 'object' && item !== null)).toBe(true)
+        expect(response.body.errors.every(item => typeof item === 'object' && item !== null)).toBe(true)
+        expect(response.body.connections.every(item => typeof item === 'object' && item !== null)).toBe(true)
+      });
+    // end errorData/allData
+    });
+    
+    describe('test: /errorData/allErrors', () => {
+      it('responds with 200 status and application/json content type', async () => {
+        await request(server)
+          .get('/errorData/allData')
+          .expect('Content-Type', /application\/json/)
+          .expect(200);
+      });
+
+      it('errors are in body of response with correct format', async () => {
+        const response = await request(server)
+          .get('/errorData/allErrors')
+        expect(Array.isArray(response.body.errors)).toBe(true)
+        expect(response.body.errors.every(item => typeof item === 'object' && item !== null)).toBe(true)
+      });
+    // end errorData/allErrors
+    });
+
+    // describe('test: /errorData/newErrors/:err_time', () => {
+    //   it('responds with 200 status and application/json content type', async () => {
+    //     await request(server)
+    //       .get('/errorData/newErrors/:err_time')
+    //       .expect('Content-Type', /application\/json/)
+    //       .expect(200);
+    //   });
+
+      // test to check if correct errors are sent according to time?
+
+    // end errorData/newErrors
+    // });
+
+    describe('test: /errorData/allServices', () => {
+      it('responds with 200 status and application/json content type', async () => {
+        await request(server)
+          .get('/errorData/allServices')
+          .expect('Content-Type', /application\/json/)
+          .expect(200);
+      });
+
+      it('services are in body of response with correct format', async () => {
+        const response = await request(server)
+          .get('/errorData/allServices')
+        expect(Array.isArray(response.body.services)).toBe(true)
+        expect(response.body.services.every(item => typeof item === 'object' && item !== null)).toBe(true)
+      });
+    // end errorData/allServices
+    });
+
+    describe('test: /errorData/allConnections', () => {
+      it('responds with 200 status and application/json content type', async () => {
+        await request(server)
+          .get('/errorData/allConnections')
+          .expect('Content-Type', /application\/json/)
+          .expect(200);
+      });
+
+      it('connections are in body of response with correct format', async () => {
+        const response = await request(server)
+          .get('/errorData/allConnections')
+        expect(Array.isArray(response.body.connections)).toBe(true)
+        expect(response.body.connections.every(item => typeof item === 'object' && item !== null)).toBe(true)
+      });
+    // end /errorData/allConnections
+    });
+
+  // end integration errorData
+  });
+// end server route tests  
+});
