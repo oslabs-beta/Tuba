549dae30da04b7cb7713d83878aa2e79ebc3b887
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2024-04-23 22:23:21 -0400

    solved initial render of state bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Home from './Pages/Home';
 import HeatMap from './Pages/HeatMap';
@@ -6,17 +6,37 @@ import Timeline from './Pages/Timeline';
 import History from './Pages/History';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getAllErrors, getServices, getConnections } from './Redux/errorSlice';
 
 
 export default function App() {
+
+    const dispatch = useDispatch();
+
+    function initialScan() {
+      let hasRun = false;
+      return () => {
+        console.log('initalScan triggered')
+        if(hasRun === false) {
+          dispatch(getAllErrors())
+          dispatch(getServices())
+          dispatch(getConnections())
+          hasRun = true;
+        }
+      }
+    }
+
+    useEffect(() => {
+      const scan1 = initialScan()
+      scan1();
+    }, [])
 
     return (
         <>
 
             <Router>
                 <Routes>
-
-
                     <Route path="/" element={<Home />} />
                     <Route path="/heatmap" element={<HeatMap />} />
                     <Route path="/timeline" element={<Timeline />} />
